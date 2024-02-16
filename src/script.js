c88f473f0b8ca3b804a6d00740444614c8f6b1ad
Tamsyn October <tamsynoctober15@gmail.com>
2024-02-16 23:34:42 +0200

    Inject the forecast HTML from JavaScript

--- src/script.js
+++ src/script.js
@@ -59,6 +59,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-row">
+          <div class="weather-forecast-col">
+            <div class="weather-forecast-day">${day}</div>
+            <div class="weather-forecast-icon">X</div>
+            <div class="weather-forecast-temps"><strong>15</strong> 12</div>
+          </div>
+        </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
+
+displayForecast();
